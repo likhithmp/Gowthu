4e3b1acb3f8e4edd746abd235c6a99b045acb9e5
acer <acer@AO-14-Z476>
2022-09-23 00:43:59 +0530

    hello

---
+++ src/meesho/Sample3.java
@@ -0,0 +1,21 @@
+package meesho;
+
+public class Sample3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+           System.out.println("hi");
+	}
+
+}
